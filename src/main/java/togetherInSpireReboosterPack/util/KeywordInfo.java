80699a05c7318496b0bcef3d2dd82d2788772355
pikcube <58404969+pikcube@users.noreply.github.com>
2025-09-28 18:32:55 -0400

    Initial Commit of Template

---
+++ src/main/java/togetherInSpireReboosterPack/util/KeywordInfo.java
@@ -0,0 +1,21 @@
+package togetherInSpireReboosterPack.util;
+
+import basemod.helpers.KeywordColorInfo;
+
+public class KeywordInfo {
+    public String ID = "";
+    public String PROPER_NAME;
+    public String DESCRIPTION;
+    public String[] NAMES;
+    public KeywordColorInfo COLOR;
+
+    public KeywordInfo() {
+    }
+
+    public void prep() {
+        for (int i = 0; i < NAMES.length; ++i)
+        {
+            NAMES[i] = NAMES[i].toLowerCase();
+        }
+    }
+}
